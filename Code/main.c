9c239826831e6a2bce66179bee50bf5630be92f1
TiagoSM <tiagosantosmes@gmail.com>
2023-04-15 19:27:13 -0300

    Adicionado operações de exponenciação, raiz quadrada e limpeza de tela após etapas

--- Code/main.c
+++ Code/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <math.h>
 
 double soma(double num1, double num2){
     return num1 + num2;
@@ -22,18 +23,27 @@ double resto(double num1, double num2){
     return (num1*num2)/100;
 }
 
+double exponenciar (double num1, double num2){
+    return (pow(num1,num2));
+}
+
+double raizQuadrada (double num1, double num2){
+    return (sqrt(num1));
+}
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
     system("color 0A");
-    int opcao;
+    int opcao, saida;
     double num1, num2, resultado;
+
+    do{
     printf("\nCalculadora de operações básicas\n\n\n");
     printf("--------------------------------\n");
     printf("Digite o valor do primeiro número: ");
     scanf("%lf", &num1);
-    printf("Digite o valor do segundo número: ");
-    scanf("%lf", &num2);
+    setbuf(stdin, NULL);
     printf("--------------------------------\n");
     printf("\nEscolha uma das operações: \n\n");
     printf("1-Soma.\n");
@@ -41,7 +51,16 @@ int main()
     printf("3-Multiplicação.\n");
     printf("4-Subtração.\n");
     printf("5-Resto de uma divisão.\n");
+    printf("6-Exponenciação.\n");
+    printf("7-Raiz Quadrada.\n");
     scanf("%d", &opcao);
+    if(opcao != 7){
+        printf("--------------------------------\n");
+        printf("Digite o valor do segundo número: ");
+        scanf("%lf", &num2);
+        setbuf(stdin, NULL);
+    }
+    setbuf(stdin, NULL);
     printf("--------------------------------\n");
     switch(opcao){
     case 1:
@@ -52,13 +71,11 @@ int main()
         if (num2==0){
             printf("\nNão é possível dividir por 0\n");
         }
-
         else {
                 resultado=dividir(num1, num2);
                 printf("\nO resultado da divisão é: %.2lf\n", resultado);
         }
         break;
-
     case 3:
         resultado=multipicar(num1, num2);
         printf("\nO resultado da multiplicação é: %.2lf\n", resultado);
@@ -71,10 +88,36 @@ int main()
         resultado=resto(num1, num2);
         printf("\nO resto da divisão é: %.2lf\n", resultado);
         break;
+    case 6:
+        resultado=exponenciar(num1, num2);
+        printf("\nO resultado da exponenciação é: %.2lf\n", resultado);
+        break;
+    case 7:
+        resultado=raizQuadrada(num1, num2);
+        printf("\nA raiz quadrada é: %f\n", resultado);
+        break;
     default:
         printf("\nEscolha uma das opções acima!!!\n\n");
 
     }
     system("pause");
+    system("cls");
+    do{
+        printf("Digite 1 para fazer mais uma conta.\n");
+        printf("Digite 2 para fechar a calculadora.\n");
+        scanf("%d", &saida);
+        setbuf(stdin, NULL);
+        switch(saida){
+            case 1:
+                system("cls");
+                break;
+            case 2:
+                system("cls");
+                break;
+            default:
+               printf("Opção Inválida!");
+        }
+        }while(saida != 1 && saida != 2);
+    }while(saida != 2);
     return 0;
 }
